8546231fc4864ad9405fa31c6276a7147ba53dc8
Anthony Medina <antmedina91@gmail.com>
2018-11-18 21:11:53 +0000

    modify app to begin exercise

--- exercises/autocomplete/src/components/App.tsx
+++ exercises/autocomplete/src/components/App.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
-import './App.css';
+import * as React from 'react';
+import { PlaceSearchResult } from './place-search-result';
+import {
+  PlaceDetails,
+  PlaceSummary,
+  fetchPlaceSummaries,
+  fetchPlaceDetails
+} from '../utils/places';
 
-interface IAppProps {
-  name: string;
+interface IAppState {
+  results: PlaceDetails[];
 }
 
-const App: React.SFC<IAppProps> = ({ name }) => (
-  <div className="App">
-    <h1>A Typescript React App by {name}</h1>
-  </div>
-);
+export default class App extends React.Component<{}, IAppState> {
+  constructor() {
+    super({});
+    this.state = {
+      results: []
+    };
+  }
 
-export default App;
+  async componentDidMount() {
+    const placeSummaries: PlaceSummary[] = await fetchPlaceSummaries('donut');
+    const results: PlaceDetails[] = await fetchPlaceDetails(
+      placeSummaries.map(p => p.place_id)
+    );
+    this.setState({ results });
+  }
+  render() {
+    const placeResults = this.state.results.map(pr => {
+      return <p key={pr.id}>{pr.name}</p>;
+      // return <PlaceSearchResult {...pr}/>;
+    });
+    return <ul className="results">{placeResults}</ul>;
+  }
+}
